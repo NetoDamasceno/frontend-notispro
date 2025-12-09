23ab1cef720953a2fa0b245b07cfbea99efc2b0e
NetoDamasceno <luiznetto13@gmail.com>
2025-12-08 21:52:29 -0300

    dark login updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -47,7 +47,7 @@ export default function Login() {
           p-6 rounded-lg md:p-0 shadow-md md:shadow-none
           "
         >
-          <h2 className="text-2xl font-semibold text-center dark:text-white">Login</h2>
+          <h2 className="text-2xl font-semibold text-center dark:md:text-white dark:sm:text-black">Login</h2>
 
           <input
             type="text"
